1ee6ec3567bf844a4096f47fd1b8593007442a7f
Lyle Hubbard <lhubbard@pdx.edu>
2021-02-15 23:30:03 -0800

    Template file should be minimally complete
    Template file uses a passed in API object that is written in the src directory.  this is where callbacks are set, and the api is defined.  The corresponding http rest movements are handled in the express file, mServiceTemplate.js

--- mServiceTemplate.js
+++ mServiceTemplate.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const path = require("path");
 const logger = require("./middleware/logger");
-
+const mservice = require("./src/API");
 //Set other essentials here
 const ex_runtime = express();
 const PORT = process.env.PORT || 7000;
@@ -34,5 +34,6 @@ ex_runtime.use(logger);
     throw "Unknown http protocol " + mservice[i].protocol + " passed to express routing!!!"
   }
 
+  
 
-ex_runtime.listen();
+ex_runtime.listen(PORT, () => console.log(`Server started on port ${PORT}`));
