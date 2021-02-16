f85271d9c16b67e8c40fc47d401d02a20f6fff80
Lyle Hubbard <lhubbard@pdx.edu>
2021-02-15 23:16:07 -0800

    added protocol checking and callback objects expressing the api

--- mServiceTemplate.js
+++ mServiceTemplate.js
@@ -8,5 +8,21 @@ const ex_runtime = express();
 const PORT = process.env.PORT || 7000;
 ex_runtime.use(logger);
 ex_runtime.listen();
-ex_runtime.get(__dir, (req, res) => {
 
+
+
+if (mservice[i].protocol === "get"){
+  ex_runtime.get(mservice[i].loc, mservice[i].cb(req, res));
+} else if (mservice[i].protocol === "post"){
+  ex_runtime.post(mservice[i].loc, mservice[i].cb(req,res));
+  
+} else if (mservice[i].protocol === "delete"){
+  ex_runtime.delete(mservice[i].loc, mservice[i].cb(req,res));
+     
+
+} else if (mservice[i].protocol === "put"){
+  ex_runtime.put(mservice[i].loc, mservice[i].cb(req,res));
+}
+else{
+  throw exception("Unknown http protocol used in express runtime routing!!!");
+}
--- middleware/logger.js
+++ middleware/logger.js
@@ -1,6 +1,6 @@
 //simple logger class that captures events and time of event, lifted from traversy
 const moment = require("moment");
-
+//note next must be called to proceed to next middleware program
 const logger = (req, res, next) =>{
   console.log(
     `${req.protocol}://${req.get('host')}${req.originUrl}: ${moment().format()}`
