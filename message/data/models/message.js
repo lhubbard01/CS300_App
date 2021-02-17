55b8d42fc9fc9e019e8f201634f5b4196846a52b
Lyle Hubbard <lhubbard@pdx.edu>
2021-02-17 01:15:26 -0800

    basic direct message schema implemented

--- message/data/models/message.js
+++ message/data/models/message.js
@@ -1,3 +1,48 @@
 const mongoose = require("mongoose")
-const Schema = mongoose.Schema
-const Message = new Schema
+const Schema = mongoose.Schema;
+
+
+
+const DMessageScehma = new Schema({
+  username: {
+    type: String,
+    required: true
+  },
+
+  datetime: {
+    type: Date,
+    default: Date.now
+  },
+
+  src: {
+    type: String,
+    required: true
+  },
+  
+  pmsg_id: {
+    type: id,
+    required: true
+  },
+
+  src_username: {
+    type: String,
+    required: true
+  },
+
+  dest: {
+    type: String,
+    required: true
+  },
+  
+  is_aggregate: {
+    type: Bool,
+    required: true
+  },
+
+  content: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = DMessage = mongoose.model("dmessage", DMessageSchema);
