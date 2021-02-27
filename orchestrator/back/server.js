de76e1e71ddb5f60654f7298790ddc51a5896a1f
Lyle Hubbard <lhubbard@pdx.edu>
2021-02-26 18:32:35 -0800

    orchestrator main class definitions

--- orchestrator/back/server.js
+++ orchestrator/back/server.js
@@ -1,5 +1,94 @@
 import {express, Router} from "express";
 const router = Router();
-router.get("/api/ports")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class AbstractService{
+  constructor(name, port){
+    this.name = name;
+    this.port = port;
+  }
+}
+
+
+class Service extends AbstractService
+{
+  constructor(name, port, listening, orch_port){
+    super(name, port);
+    this.listening = listening;
+    this.orch = orch;
+  }
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class ServerMain extends AbstractService{
+  constructor(name, port){
+    super(name, port);
+    this.lookup = Map();
+    this.availablePorts = Map();
+  };
+};
+
+
+
+
+
+server = ServerMain("ServiceOrchestrator", 5020);
+
+router.get("/api/ports", async (req, res) => {
+const { caller, ports, authtoken, target } = req.body;
+let val = await authenticateToken(authToken);
+await seek(target);
+
+
+
 router.get("/api/services")
 router.get("/api/workload")
+
