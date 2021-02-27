58e14c0d846bafc6cd547522b38011569903a1ba
Lyle Hubbard <lhubbard@pdx.edu>
2021-02-26 18:54:51 -0800

    register post and delete implemented

--- orchestrator/back/server.js
+++ orchestrator/back/server.js
@@ -21,15 +21,6 @@ const router = Router();
 
 
 
-
-
-
-
-
-
-
-
-
 class AbstractService{
   constructor(name, port){
     this.name = name;
@@ -50,45 +41,52 @@ class Service extends AbstractService
 };
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 class ServerMain extends AbstractService{
   constructor(name, port){
     super(name, port);
     this.lookup = Map();
     this.availablePorts = Map();
+    this.addService = this.addService.bind(this);
+    this.getService = this.getService.bind(this);
   };
+  
+
+  addService(serviceName, servicePort){
+    if (!this.lookup.get(serviceName){
+      this.lookup.put(serviceName, servicePort);
+      return true;
+    }
+    return false;
+  }
+  
+  getService(serviceName){
+    return this.lookup.get(serviceName);
+  }
 };
 
-
-
-
-
 server = ServerMain("ServiceOrchestrator", 5020);
+router.post("/api/register", (req, res) => {
+  const { caller, callerListenPort } = req.body;
+  let outcome = server.addService(caller, callerListenPort);
+  if (outcome){
+    res.body = {}
+  }
+  else{
+    res.body = {}
+  }
+});
 
-router.get("/api/ports", async (req, res) => {
-const { caller, ports, authtoken, target } = req.body;
-let val = await authenticateToken(authToken);
-await seek(target);
 
 
 
-router.get("/api/services")
-router.get("/api/workload")
+/*router.get("/api/ports", async (req, res) => {
+  const { caller, ports, authtoken, target } = req.body;
 
+//let val = await authenticateToken(authToken);
+await seek(target);*/
+
+
+
+//router.get("/api/services")
+//router.get("/api/workload")
+//router.delete("/api/register", (req, res) => {});
