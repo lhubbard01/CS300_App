396c3c1a64f098061fcaf913920053479be1d38f
Lyle Hubbard <lhubbard@pdx.edu>
2021-03-09 08:32:01 -0800

    login minimally functional, receives signal

--- login/back/index.js
+++ login/back/index.js
@@ -1,25 +1,22 @@
 const express = require("express");
 const https = require("http");
-const Agent = https.Agent;
-const app = express();
-//networking definitions and startup
-const orchestratorPort = 5020;
-//Declared routes and Routers
-//const createRouter = require( "./routes/createRouter" );
-//const loginRouter  = require("./routes/loginRouter" );
-//app.use(createRouter);
-//app.use(loginRouter);
+const properties = require("./package.json");
+const SignupRouter  = require("./routes/signup");
+const manifest = properties.manifest;
 
+
+const app = express();
+app.use(SignupRouter);
 
 //networking definitions and startup
 const port = process.env.PORT || 5040;
 
-const data = JSON.stringify( {
-  'name': 'Login',
-  'port': port
-});
 
-const options = {
+async function announceSpinup(){
+  const data = JSON.stringify( {"name": manifest.name, "manifest": manifest
+  });
+
+  const options = {
   hostname: "localhost",
   port: 5020,
   path: "/api/register",
@@ -29,25 +26,26 @@ const options = {
       'Content-Type' : 'application/json',
       'Content-Length': data.length
     },
-    agent: new Agent({ rejectUnauthorized: false })
-};
+    //agent: new Agent({ rejectUnauthorized: false })
+  };
+  console.log("initiating request send off");
 
- 
-console.log("initiating request send off");
-
-const req = https.request(options, res => {
+  const req = https.request(options, res => {
   console.log(`status code : ${res.statusCode}`)
   res.on("data", d => {
     process.stdout.write(d)
-    })
-})
-
-req.on("error", error => {
-    console.error(error)
+    });
   })
 
-console.log("request created");
-req.write(data);
+  req.on("error", error => {
+    console.error(error)
+  });
+
+  console.log("request created");
+  req.write(data);
+  req.end();
+}
+announceSpinup();
 
 app.listen(port);
 console.log("Login listening on port 5040");
