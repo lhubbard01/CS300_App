0a212cf912eab570841438a119d78a1e67e4683d
Lyle Hubbard <lhubbard@pdx.edu>
2021-03-09 08:31:11 -0800

    login component sends data and is received serverside\n\ncurrent all of the component is handled in index. However, the signup functionality is mvp atm

--- login/front/src/index.js
+++ login/front/src/index.js
@@ -1,4 +1,123 @@
-import React from "react";
-import  ReactDOM from "react-dom";
-import App from "./App";
+const {React, Component} = require("react");
+const  ReactDOM =  require("react-dom");
+console.log(Component);
+
+class Header extends Component{
+  constructor(props){
+    super(props);
+    this.state = {title: props.title}
+  };
+  render(){
+    return (
+      <h1 style={{background: "red"}}> {this.state.title}</h1>
+
+
+    );
+  }
+};
+
+class SignupBox extends Component{
+  constructor(props){
+    super(props);
+
+    this.state = {
+      unvalue: "", pwvalue: "", emvalue :""
+    };
+    this.signupRoute = this.signupRoute.bind(this); 
+    this.handleSignup = this.handleSignup.bind(this);
+
+    this.handleChangeEmail = this.handleChangeEmail.bind(this);
+    this.handleChangePassword = this.handleChangePassword.bind(this);
+    this.handleChangeUsername = this.handleChangeUsername.bind(this);
+    
+  }
+
+
+  handleChangeUsername(event){
+    this.setState( { unvalue : event.target.value}); 
+  }
+  
+  handleChangePassword(event){
+    this.setState({pwvalue: event.target.value});
+  }
+
+  handleChangeEmail(event){
+    this.setState({emvalue: event.target.value } );
+  }
+
+  async signupRoute(outbound){
+    return fetch("/api/signup", outbound).then(data => data.json());
+  }
+  
+
+  async handleSignup(event){
+    //event.PreventDefault();
+    const data = JSON.stringify({ "username" : this.state.unvalue,
+              "email": this.state.emvalue,
+              "password": this.state.pwvalue});
+
+    const requestOptions = {
+      method: "POST",
+      headers: {"Content-Type": "application/json", "Content-Length": data.length},
+      body: data
+    };
+    alert(JSON.stringify(requestOptions));
+    const res = await this.signupRoute(JSON.stringify(requestOptions));
+
+    alert(res);
+  }
+
+  render(){
+    return ( <div className="Signup">
+
+      <div className="SignupBox">
+      <h2>Sign Up</h2>
+        <form>
+
+          <value>Username</value><br/>
+          <input type="text" 
+            value={this.state.unvalue}
+            onChange={this.handleChangeUsername} /><br/>
+
+          <value>Email</value><br/>
+          <input type="text"
+            value={this.state.emvalue} 
+            onChange={this.handleChangeEmail} /><br/>
+
+          <value>Password</value><br/>
+          <input type = "password" 
+            value={this.state.pwvalue}
+            onChange={this.handleChangePassword} /><br/>
+          <input type="submit" onClick={this.handleSignup}/><br/>
+        </form>
+      </div>
+    </div>
+    );
+  }
+};
+
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+    }
+  }
+  render(){
+
+
+    return (
+      <div className="LoginPage"  >
+        <Header title="Login" />
+        <SignupBox />
+      </div>
+      );
+  }
+};
+/*class LoginBox extends REeact.Compoent { 
+  constructor(props){
+    super(props);
+    this.state = {}*/
+
+
+
 ReactDOM.render( <App /> ,  document.getElementById("root"));
